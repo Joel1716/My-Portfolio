39a3abbab4b28af10506344b0e3cb9db79b77ddc
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2025-02-26 00:52:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -50,3 +50,85 @@ icons.addEventListener("click", () => {
 navEl.addEventListener("click", () => {
   headerEl.classList.remove("open");
 });
+/// TECH LOGOS DISPLAY ON PAGE
+const logos = document.querySelector(".js-logos");
+function logoCreation() {
+  let imageHTML = "";
+  let logoSlideHTML = "";
+  let i = 0;
+  tech.forEach((eachImage) => {
+    imageHTML += `   <img
+      src="${eachImage.img}"
+   />`;
+  });
+  while (i < 2) {
+    logoSlideHTML += `<div class="logos-slide">
+    ${imageHTML}
+          </div>`;
+    i++;
+  }
+  logos.innerHTML = logoSlideHTML;
+}
+logoCreation();
+
+//// PROFILES DISPLAY ON PAGE
+function profileCreation() {
+  const listOfProjects = document.querySelector(".js-list-of-projects");
+  let languageHTML = "";
+  let profileHTML = "";
+  profiles.forEach((profile) => {
+    languageHTML = "";
+    profile.languages.forEach((language) => {
+      languageHTML += `<div>${language}</div>`;
+    });
+    profileHTML += ` 
+      <div class="each-project">
+         <div class="whatsapp-image-container">
+              <img class="whatsapp-image" src="${profile.img}" alt="" />
+          </div>
+          <div class="project-content">
+              <h2>${profile.title}</h2>
+              <div class="project-type">${profile.type}</div>
+              <p class="project-message">${profile.message}</p>
+            <div class="view-tech">
+                <h2>Technologies Used</h2>
+              <div class="projects-tech">${languageHTML}</div>
+              <div class="view-project">
+                  <a
+                    href="${profile.link}"
+                    target="_blank"
+                    >View</a
+                  >
+              </div>
+            </div>
+          </div>;
+      </div>`;
+  });
+  listOfProjects.innerHTML = profileHTML;
+}
+profileCreation();
+///// RECEIVING INPUTS
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form from redirecting
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    fetch("https://formspree.io/f/manqgknv", {
+      method: "POST",
+      body: formData,
+      headers: { Accept: "application/json" },
+    })
+      .then((response) => {
+        if (response.ok) {
+          document.querySelector(".status-message").style.display = "block";
+          form.reset(); // Clear the form
+        } else {
+          alert("Error sending message. Please try again.");
+        }
+      })
+      .catch((error) => alert("Error: " + error));
+  });
