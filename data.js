8288c0abd3477bba76534b9c817243fee0574096
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2025-02-26 00:53:01 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,74 @@
+export const tech = [
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jasmine/jasmine-original-wordmark.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+  },
+];
+
+export const profiles = [
+  {
+    img: "/images/Project-1.PNG",
+    title: "Whatsapp Project",
+    type: "Mobile",
+    message:
+      "A replica of the Whatsapp mobile that gives you the freedom to click on any profile, send as many messages as you want with the exact time, be able to go back and repeat for other profiles but the beauty is the ability to go to a previous profile and meet the exact messages that where sent there.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://whatsapp-page-nine.vercel.app/",
+  },
+  {
+    img: "/images/Project-3.PNG",
+    title: "CGPA-GPA Calculator",
+    type: "Web",
+    message:
+      "As a Student, determining the range of your score is something that has been wished upon to remove tension. Well, this is where my app comes in, it allows you to check either your CGPA(Input each semester then click enter, when you are done, calculate!!) or GPA(Inputting the values and calculate) with ease.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://cgpa-gpa-calculator.vercel.app/",
+  },
+  {
+    img: "/images/Project-2.PNG",
+    title: "Fylo Landing Page",
+    type: "Web",
+    message:
+      "A Landing page that shows the benefits of using fylo to securely store all files uploaded with no room for unauthorised user access and as a bonus, it allows faster data access. It shows positive testimonials from other clients while also allowing another user to sign up with their email.",
+    languages: ["HTML", "CSS"],
+    link: "https://fylo-landing-page-three-zeta.vercel.app/",
+  },
+  {
+    img: "/images/Project-4.PNG",
+    title: "Mortgage Calculator",
+    type: "Web",
+    message:
+      "Always wanted a faster and easier way to calculate your mortgage? Well, we have your answer. Introducing our mortgage calculator which allows you to calculate your mortage seamlessly with broken down monthly payments and the total payments over a year without you breaking a sweat ‼",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://mortgage-calculator-lac.vercel.app/",
+  },
+  {
+    img: "/images/Project-5.PNG",
+    title: "Base-Apparel",
+    type: "Web",
+    message:
+      " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore veritatis ex accusamus, corporis officia. Repudiandae aperiam voluptatem architecto esse a adipisci fugit veniam aliquid officia, id, iusto, aut neque.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://joel1716.github.io/Base-apparel/",
+  },
+  {
+    img: "/images/Project-1.PNG",
+    title: "Whatsapp Project",
+    type: "Mobile",
+    message:
+      "A replica of the Whatsapp mobile that gives you the freedom to click on any profile, send as many messages as you want with the exact time, be able to go back and repeat for other profiles but the beauty is the ability to go to a previous profile and meet the exact messages that where sent there.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://whatsapp-page-nine.vercel.app/",
+  },
+];
