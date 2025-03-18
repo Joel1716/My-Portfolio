8655cbe29b05cdf42480aefabda4c9328346c6bf
Joel Ohikhena <joelohikhena12@gmail.com>
2025-03-18 01:53:57 +0100

    Portfolio

---
+++ data.js
@@ -0,0 +1,74 @@
+export const tech = [
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jasmine/jasmine-original-wordmark.svg",
+  },
+  {
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+  },
+];
+
+export const profiles = [
+  {
+    img: "/images/Project-1.PNG",
+    title: "Whatsapp Replica",
+    type: "Mobile",
+    message:
+      "WhatsApp Replica is an interactive messaging simulation that reproduces the WhatsApp mobile experience. Users can select different profiles, exchange messages with accurate timestamps, and navigate between conversations while maintaining message history. The application preserves conversation states across sessions, allowing users to return to any previous chat and find their message history intact. This project demonstrates my proficiency in frontend development, state management that mirror popular applications.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://whatsapp-page-nine.vercel.app/",
+  },
+  {
+    img: "/images/countries.PNG",
+    title: "Global Info",
+    type: "Web",
+    message:
+      "Global Info is an interactive web application that displays countries worldwide with their flags and essential information. Clicking on any country reveals detailed information about that nation. Currently built with JSON data, the project will be enhanced with RESTful API integration in future updates. This showcase demonstrates my skills in interactive UI development, data manipulation, and responsive design implementation.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: " https://joel1716.github.io/Country-generation-with-API/",
+  },
+  {
+    img: "/images/Project-3.PNG",
+    title: "CGPA-GPA Calculator",
+    type: "Web",
+    message:
+      "As a Student, determining the range of your score is something that has been wished upon to remove tension. Well, this is where my app comes in, it allows you to check either your CGPA(Input each semester then click enter, when you are done, calculate!!) or GPA(Inputting the values and calculate) with ease.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://cgpa-gpa-calculator.vercel.app/",
+  },
+  {
+    img: "/images/Project-2.PNG",
+    title: "Fylo Landing Page",
+    type: "Web",
+    message:
+      "A Landing page that shows the benefits of using fylo to securely store all files uploaded with no room for unauthorised user access and as a bonus, it allows faster data access. It shows positive testimonials from other clients while also allowing another user to sign up with their email.",
+    languages: ["HTML", "CSS"],
+    link: "https://fylo-landing-page-three-zeta.vercel.app/",
+  },
+  {
+    img: "/images/Project-4.PNG",
+    title: "Mortgage Calculator",
+    type: "Web",
+    message:
+      "Always wanted a faster and easier way to calculate your mortgage? Well, we have your answer. Introducing our mortgage calculator which allows you to calculate your mortage seamlessly with broken down monthly payments and the total payments over a year without you breaking a sweat ‼",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://mortgage-calculator-lac.vercel.app/",
+  },
+  {
+    img: "/images/Project-5.PNG",
+    title: "Base-Apparel",
+    type: "Web",
+    message:
+      " It is a responsive component that provides real-time validation for email inputs. It displays error messages when users enter incorrectly formatted email addresses, enhancing user experience through immediate feedback. This solution demonstrates my attention to detail in form validation.",
+    languages: ["HTML", "CSS", "Javascript"],
+    link: "https://joel1716.github.io/Base-apparel/",
+  },
+];
